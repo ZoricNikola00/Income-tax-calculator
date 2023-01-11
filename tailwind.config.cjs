6ac48a773c9199c9391e3b8dadfd6915d230e0f2
ZoricNikola00 <zoriclfc@gmail.com>
2023-01-11 16:13:47 +0100

    Add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@ module.exports = {
 	content: ['./src/**/*.{js,jsx}', './public/index.html'],
 	theme: {
 		extend: {
-
+            colors:{
+                primary:'#3e7f94',
+                secondary:'#7a8c91'
+            }
 		},
 	},
 	plugins: [],
