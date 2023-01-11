8b5cbebd8417ec763f32380d2201671c502ff6c7
ZoricNikola00 <zoriclfc@gmail.com>
2023-01-11 16:22:41 +0100

    Created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {useState}from 'react'
+import { FaHouseUser } from 'react-icons/fa';
 
 const App = () => {
     const [isForm,setIsForm]=useState(true)
@@ -8,7 +9,12 @@ const App = () => {
         period:'Hourly'
     })
   return (
-    <div>App</div>
+    <div className='mt-[50px] h-fit flex justify-center items-center flex-col'>
+        <div className='customShadow text-primary mb-10 w-[90%] md:-[500px] lg:w-[600px] flex items-center justify-center p-4 bg-white rounded-lg'>
+            <FaHouseUser className='text-3xl md:text-4xl'/>
+            <h1 className='ml-3 font-bold text-xl md:text-2xl'>Income Tax Calculator</h1>
+        </div>
+    </div>
   )
 }
 
