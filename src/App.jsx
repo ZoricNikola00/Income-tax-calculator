2f97f8d71e7e36f68b3cb05c58772b38fc13c5ff
ZoricNikola00 <zoriclfc@gmail.com>
2023-01-11 16:09:55 +0100

    Created states for formData and check form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useState}from 'react'
 
 const App = () => {
+    const [isForm,setIsForm]=useState(true)
+    const [formData, setFormData]=useState({
+        type:'',
+        income:0,
+        period:'Hourly'
+    })
   return (
     <div>App</div>
   )
