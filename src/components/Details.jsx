62079f361c71e70ef145fdc5e490e78d993dc813
ZoricNikola00 <zoriclfc@gmail.com>
2023-01-11 17:48:11 +0100

    Set states and constants for calculation

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Details = () => {
-  return (
+const Details = ({formData}) => {
+    const tax=0.4 //random tax
+    const selectedIncome=+formData.income
+    const [calculatedGrossIncome, setCaclulatedGrossIncome]=useState({
+        Hourly:0,
+        Daily:0,
+        Weakly:0,
+        Quarteryl:0,
+        Monthly:0,
+        Annualy:0
+    })
+
+    return (
     <div>Details</div>
   )
 }
